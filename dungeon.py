ca9561b4dc6cd39196490a990a590bb9e2c050f9
unknown <agarrett@D6MQTM1.olin.edu>
2013-11-21 20:04:23 -0500

    Dungeon now generates maps reliably with non-intersecting rooms. Graphical display using 'x' and ' '.

--- dungeon.py
+++ dungeon.py
@@ -3,9 +3,11 @@ import random
 class Room(object):
     """Represents a room in a map grid"""
 
-    def __init__ (self, w=10, h=10):
+    def __init__ (self, w=3, h=3, x='None', y='None'):
         self.w = w
         self.h = h
+        self.x = x
+        self.y = y
 
     def __str__ (self):
         return 'Room object with bottom left corner at %d,%d, width %d, and height %d' % (self.x, self.y, self.w, self.h)
@@ -15,15 +17,31 @@ class Map(object):
     """Represents a map in points"""
 
 
-    def __getitem__ (self, key):
-        ####THIS DOES NOT WORK YET. NEED TO MAKE __getitem__ AND __setitem__.
-
-    def __init__ (self, w=100, h=100):
-        for x in range(w):
-            for y in range(h):
-                self[x][y].blocked = True
+    def __init__ (self, w=20, h=20):
+        
         self.w = w
         self.h = h
+        init=dict()
+        for i in range(w):
+            for j in range(h):
+                init[i+1,j+1]='blocked'
+        self.__dict__.update(init)
+
+        
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
+
+    def __contains__(self, key):
+        return key in self.__dict__
+    
+    def __len__(self):
+        return len(self.__dict__)
 
     def __str__ (self):
         """prints graphical representation of map"""
@@ -31,30 +49,48 @@ class Map(object):
         for y in range(self.h):
             print ''
             for x in range(self.w):
-                if map[x][y].blocked:
-                    print 'x'
+                if self.__dict__[(x+1,y+1)] == 'blocked':
+                    print 'x',
                 else:
-                    print ' '
+                    print ' ',
+        return ''
 
 
 
 
 def generate_position(room, map):
     """Generates x and y position for bottom left corner of room"""
-    x_pos=random.randint(0, map.w-room.w)
-    y_pos=random.randint(0, map.h-room.h)
+    x_pos=random.randint(1, map.w-room.w)
+    y_pos=random.randint(1, map.h-room.h)
 
     for i in range(room.w):
         for j in range(room.h):
-            if not h[x_pos + i][y_pos + j]:
+            if map[x_pos + i,y_pos + j] != 'blocked':
                 return generate_position(room,map)
     return (x_pos,y_pos)
+
+def update_map(rooms, map):
+    for room in rooms:
+        x_pos,y_pos =generate_position(room, map)
+        room.x= x_pos
+        room.y= y_pos
+        for i in range(room.w):
+            for j in range(room.h):
+                map[x_pos+i,y_pos+j]='not blocked'
     
 
 
 def main():
     maps=Map()
+    room=Room()
+    room2=Room(6,4)
+    room3=Room(2,8)
+    rooms=[room, room2, room3]
+    update_map(rooms,maps)
     print maps
+    print room
+    print room2
+    print room3
     
 if __name__ == '__main__':
     main()
