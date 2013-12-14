c294975e350cd288fc079d4f6e550c74361ca968
unknown <mzhu@1VYYYN1.olin.edu>
2013-12-14 12:53:52 -0500

    Now generates a map of room connections. TODO: actually implement the questing.

--- dungeon.py
+++ dungeon.py
@@ -21,17 +21,19 @@ class Room(object):
         self.h = random.randint(3, 10)
         self.x = x
         self.y = y
+        self.id = None
 
     def __str__(self):
-        return 'Room object with top left corner at %d,%d, width %d, and height %d' % (self.x, self.y, self.w, self.h)
+        return 'At (%d,%d), with width %d, and height %d' % (self.x, self.y, self.w, self.h)
 
 
 class Dungeon(dict):
     """Represents a map in points"""
 
-    def __init__(self, w=20, h=20):
+    def __init__(self, w=20, h=20, connections=None, start_room=None):
         self.w = w
         self.h = h
+        self.connections = {}
         for i in range(w):
             for j in range(h):
                 self[i, j] = WALL
@@ -49,9 +51,12 @@ class Dungeon(dict):
                     layout += ' '
                 elif self[x, y] == TUNNEL:
                     layout += '-'
+                else:
+                    layout += self[x,y]
             layout += '\n'
 
         return layout
+
 
 
     def get_valid_position(self, room):
@@ -70,23 +75,38 @@ class Dungeon(dict):
 
 
     def place_rooms(self, rooms):
-        for room in rooms:
-            room.x, room.y = self.get_valid_position(room)
-            for i in range(-1, room.w + 1):
-                for j in range(-1, room.h + 1):
-                    if i == -1 or j == -1 or i == room.w or j == room.h:
-                        self[room.x + i, room.y + j] = PERIMETER
-                    else:
-                        self[room.x + i, room.y + j] = ROOM
-
-
-    def which_room(self, coord, rooms):
-        x, y = coord
         for k in range(len(rooms)):
-            left_edge = rooms[k].x
-            right_edge = rooms[k].x + rooms[k].w
-            top_edge = rooms[k].y
-            bottom_edge = rooms[k].y + rooms[k].h
+            rooms[k].id = k
+            self.connections[rooms[k]] = set()
+            rooms[k].x, rooms[k].y = self.get_valid_position(rooms[k])
+            for i in range(-1, rooms[k].w + 1):
+                for j in range(-1, rooms[k].h + 1):
+                    if i == -1 or j == -1 or i == rooms[k].w or j == rooms[k].h:
+                        self[rooms[k].x + i, rooms[k].y + j] = PERIMETER
+                    else:
+                        self[rooms[k].x + i, rooms[k].y + j] = str(rooms[k].id)
+
+    def make_connection(self, room):
+        if hasattr(self.make_connection, 'last_room'):
+            self.connections[self.make_connection.last_room].add(room)
+            self.connections[room].add(self.make_connection.last_room)
+        self.make_connection.__func__.last_room = room
+
+    def print_connections(self):
+        for room, adjacent in self.connections.items():
+            print str(room.id) + ': ',
+            for adj_room in adjacent:
+                print str(adj_room.id) + ' ',
+            print
+
+    def which_room(self, coord):
+        x, y = coord
+        rooms = self.connections.keys()
+        for k in range(len(rooms)):
+            left_edge = rooms[k].x - 1
+            right_edge = rooms[k].x + rooms[k].w + 1
+            top_edge = rooms[k].y - 1 
+            bottom_edge = rooms[k].y + rooms[k].h + 1
             if left_edge <= x <= right_edge and top_edge <= y <= bottom_edge:
                 return k
         return False
@@ -110,37 +130,40 @@ class Dungeon(dict):
                 return False
             if self[coord] == TUNNEL:
                 return False
+            if self[coord] == ROOM or self[coord] == PERIMETER: 
+                room = self.connections.keys()[self.which_room(coord)]
+                self.make_connection(room)
+                try:
+                    self.tunnel.unconnected.remove(room)
+                except ValueError:
+                    pass
             new_dungeon[coord] = TUNNEL
         return new_dungeon
 
 
-    def tunnel(self, coord, unconnected):
+    def tunnel(self, coord):
+        if not hasattr(self.tunnel, 'unconnected'):
+            self.tunnel.__func__.unconnected = self.connections.keys()
+        if self.tunnel.unconnected == []:
+            print "Reached all rooms"
+            return self
         global DIRECTIONS
         random.shuffle(DIRECTIONS)
-        if unconnected == []:
-            return self
         for direction in DIRECTIONS:
-            if self[coord] == ROOM or self[coord] == PERIMETER:
-                try:
-                    unconnected.pop(self.which_room(coord, unconnected))
-                except IndexError:
-                    pass
             new_dungeon = self.valid_tunnel(coord, direction)
             if new_dungeon:
                 self = new_dungeon
                 new_coord = tuple(
                     n + TUNNEL_LENGTH * dn for n, dn in zip(coord, direction))
-                self = self.tunnel(new_coord, unconnected)
+                self = self.tunnel(new_coord)
         return self
 
 
-    def connect_rooms(self,rooms):
-        start_room = random.choice(rooms)
-        length = 3
-        x = random.randint(start_room.x, start_room.x + start_room.w)
-        y = random.randint(start_room.y, start_room.y + start_room.h)
-        unconnected = copy.copy(rooms)
-        self = self.tunnel((x, y), unconnected)
+    def connect_rooms(self):
+        self.start_room = random.choice(self.connections.keys())
+        x = random.randint(self.start_room.x, self.start_room.x + self.start_room.w)
+        y = random.randint(self.start_room.y, self.start_room.y + self.start_room.h)
+        self = self.tunnel((x, y))
         return self
 
 
@@ -148,9 +171,9 @@ def main():
     dungeon = Dungeon(50, 50)
     rooms = [Room() for k in range(10)]
     dungeon.place_rooms(rooms)
-    dungeon = dungeon.connect_rooms(rooms)
+    dungeon = dungeon.connect_rooms()
     print dungeon
-
-
+    dungeon.print_connections()
+    #print dungeon.connections
 if __name__ == '__main__':
     main()
